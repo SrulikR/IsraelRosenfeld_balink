1da0278b12de7566896aa11e9071dd283a1adf98
Srulik <srulikr@gmail.com>
2021-11-14 02:38:48 +0200

    remove real url from DB

--- models/personDb.js
+++ models/personDb.js
@@ -2,7 +2,7 @@ const postgres = require('postgres')
 //url to my local DB
 const sql = postgres('postgres://postgres:@localhost:5432/balink')
 //url from db uploadd to heroku, bud not allowed use SSL without a pay program 
-//const sql = postgres('postgres://ubwlddlbzpzxcw:6580abc448d29d2fc9025f281155452000189a312b43a68316ba6b2359255440@ec2-3-225-30-189.compute-1.amazonaws.com:5432/d1cvcek1ikpc1m',{ssl:true});
+//const sql = postgres('postgres://"URL",{ssl:true});
 
 
 async function createPerson(data){ 
